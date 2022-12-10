2deb1c6f1fe518fc26c6489562b5d4bb4549876f
666EGOR777 <teteva3@gmail.com>
2022-12-10 09:56:31 +0300

    make_db_scripts

---
+++ Task05/data_add.sql
@@ -0,0 +1,16 @@
+INSERT INTO users (first_name, last_name, email, gender, occupation_id) VALUES
+
+('Maksim', 'Negrya', 'negmaksim@gmail.com', 'male', 1), 
+('Egor', 'Nikishkin', 'nikishkin@gmail.com', 'male', 2),
+('Maksim', 'Nuyanzin', 'nuyanzin@gmail.com', 'male', 3),
+('Alina', 'Pomelova', 'pomelova@gmail.com', 'female', 4),
+
+INSERT INTO movies (title, year) VALUES
+('VORONINbl: vozvrashenie', 2021),
+('daer ot yrt', 2022),
+('Terminator:99', 2021);
+
+INSERT INTO ratings (user_id, movie_id, rating) VALUES
+(3, 8, 4.3),
+(2, 9, 4.0),
+(1, 321, 0.0);
